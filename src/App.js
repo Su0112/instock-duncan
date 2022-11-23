47b327f48fa410d24e5f060039abf541c0e3bfae
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-11-22 20:36:28 -0800

    add axios response data for warehouseDetails page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Header from "./components/header/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import WarehouseDetails from "./components/warehouseDetails/WarehouseDetails";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header />
-      <Routes>
-        {/* <Route path='' element={}/>
+        <Header />
+        <Routes>
+          {/* <Route path='' element={}/>
         <Route path='' element={}/> */}
-      </Routes>
-      {/* <Footer /> */}
+          <Route path="/:warehouseId" element={<WarehouseDetails />} />
+        </Routes>
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
