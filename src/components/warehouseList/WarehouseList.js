3a641c51126d6abc4bf0faa5a6b437e886647c4a
Su Shruthi <shruthacm@gmail.com>
2022-11-24 02:29:13 -0800

    added functionalities but with errors

--- src/components/warehouseList/WarehouseList.js
+++ src/components/warehouseList/WarehouseList.js
@@ -6,6 +6,10 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import sortIcon from "../../assets/Icons/sort-24px.svg";
+import chevronRightIcon from "../../assets/Icons/chevron_right-24px.svg";
+import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../assets/Icons/edit-24px.svg";
 
 const URL = "http://localhost:8080/warehouses/";
 export default function WarehouseList() {
@@ -52,40 +56,106 @@ export default function WarehouseList() {
             <div className="warehouses__list-content">WAREHOUSE</div>
             <div className="warehouses__list-content">ADDRESS</div>
             <div className="warehouses__list-content">CONTACT NAME</div>
-            <div className="warehouses__list-content">CONTACT INFROMATION</div>
+            <div className="warehouses__list-content">CONTACT INFORMATION</div>
             <div className="warehouses__list-content">ACTIONS</div>
           </div>
-          {}
-          <div className="warehouses__list-container">
-            <div className="warehouses__list-container-wrap">
-              <div className="list__location-address">
-                <div className="list__location-container">
-                  <div className="list__location-title">WAREHOUSE{}</div>
-                  <div className="list__location-info"></div>
-                </div>
-                <div className="list__address-container">
-                  <div className="list__address-title">ADDRESS</div>
-                  <div className="list__address-info"></div>
-                </div>
-              </div>
+          {warehouse.length > 0 ? (
+            <section className="warehouses__list-wrapper">
+              {warehouse.map((warehouse) => (
+                <div className="warehouses__list-container" key={warehouse.id}>
+                  <div className="warehouses__list-container-wrap">
+                    <div className="list__location-address">
+                      <div className="list__location-container">
+                        <div className="list__location-title">
+                          <p className="list__location-titleText">WAREHOUSE</p>
+                          <img
+                            className="warehouses__list-sortIcon"
+                            src={sortIcon}
+                            alt="sort icon"
+                          />
+                        </div>
+                        <div className="list__location-info-wrapper">
+                          <div className="list__location-info">
+                            <p className="list__location-infoText">
+                              {warehouse.warehouse_name}
+                            </p>
+                          </div>
 
-              <div className="list__name-container">
-                <div className="list__name-contact">
-                  <div className="list__name-title">CONTACT NAME</div>
-                  <div className="list__name-info"></div>
-                </div>
-                <div className="list__contact-container">
-                  <div className="list__contact-title">CONTACT INFROMATION</div>
-                  <div className="list__contact-info"></div>
-                </div>
-              </div>
-            </div>
+                          <div className="list__location-info">
+                            <img src={chevronRightIcon} alt="right Icon" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="list__address-container">
+                        <div className="list__address-title">
+                          <p className="list__address-titleText">ADDRESS</p>
+                          <img
+                            className="warehouses__list-sortIcon"
+                            src={sortIcon}
+                            alt="sort icon"
+                          />
+                        </div>
+                        <div className="list__address-info">
+                          <p className="list__address-infoText">
+                            {warehouse.address}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="list__name-contact">
+                      <div className="list__name-container">
+                        <div className="list__name-title">
+                          <p className="list__name-titleText">CONTACT NAME</p>
+                          <img
+                            className="warehouses__list-sortIcon"
+                            src={sortIcon}
+                            alt="sort icon"
+                          />
+                        </div>
+                        <div className="list__name-info">
+                          <p className="list__name-infoText">
+                            {warehouse.contact_name}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="list__contact-container">
+                        <div className="list__contact-title">
+                          <p className="list__contact-titleText">
+                            CONTACT INFORMATION
+                          </p>
+                          <img
+                            className="warehouses__list-sortIcon"
+                            src={sortIcon}
+                            alt="sort icon"
+                          />
+                        </div>
+                        <div className="list__contact-info">
+                          <p className="list__contact-infoText">
+                            {warehouse.contact_phone} {warehouse.contact_email}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-            <div className="list__actions-container">
-              <div className="list__actions-delete"></div>
-              <div className="list__actions-edit">edit</div>
-            </div>
-          </div>
+                  <div className="warehouses__actions-container">
+                    <div className="warehouses__actions-buttons">
+                      <button className="warehouses__actions-button">
+                        <img src={deleteIcon} alt="delete icon" />
+                      </button>
+                      <button className="warehouses__actions-button">
+                        <img src={editIcon} alt="edit icon" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </section>
+          ) : (
+            <section className="warehouseList__container">
+              <p className="warehouseList__no-data">No warehouse</p>
+            </section>
+          )}
         </div>
       </div>
     </section>
