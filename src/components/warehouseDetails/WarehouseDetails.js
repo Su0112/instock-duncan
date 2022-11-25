a03a9a7f64f5100b13b0ca8733ab317ce6d34b08
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-11-25 15:02:41 -0800

    added core style

--- src/components/warehouseDetails/WarehouseDetails.js
+++ src/components/warehouseDetails/WarehouseDetails.js
@@ -8,45 +8,8 @@ import Back from "../../assets/Icons/arrow_back-24px.svg";
 import SortIcon from "../../assets/Icons/sort-24px.svg";
 import DeleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import Chevron from "../../assets/Icons/chevron_right-24px.svg";
-
+import { ReactComponent as Icon } from "../../assets/Icons/edit-white.svg";
 const URL = "http://localhost:8080/warehouses/";
-
-const viewportContext = React.createContext({});
-
-const ViewportProvider = ({ children }) => {
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const [height, setHeight] = React.useState(window.innerHeight);
-  const handleWindowResize = () => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-  };
-
-  React.useEffect(() => {
-    window.addEventListener("resize", handleWindowResize);
-    return () => window.removeEventListener("resize", handleWindowResize);
-  }, []);
-
-  return (
-    <viewportContext.Provider value={{ width, height }}>
-      {children}
-    </viewportContext.Provider>
-  );
-};
-
-const useViewport = () => {
-  const { width, height } = React.useContext(viewportContext);
-  return { width, height };
-};
-
-const MobileComponent = () => <p>"Hmmm... Why is your screen so small?"</p>;
-const DesktopComponent = () => <p>"Wow, your screen is big!"</p>;
-
-const MyComponent = () => {
-  const { width } = useViewport();
-  const breakpoint = 620;
-
-  return width < breakpoint ? <MobileComponent /> : <DesktopComponent />;
-};
 
 export default function WarehouseDetails() {
   const [warehouse, setWarehouse] = useState();
@@ -65,7 +28,6 @@ export default function WarehouseDetails() {
             .get(URL + warehouseId + "/inventories")
             .then((response) => {
               setInventories(response.data);
-              //   console.log(inventories);
             })
             .catch((error) => console.log(error));
         } else {
@@ -73,56 +35,73 @@ export default function WarehouseDetails() {
         }
       })
       .catch((error) => console.log(error));
-    // console.log(warehouse);
   }, [params]);
 
-  return (
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 767;
+
+  React.useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+
+    // Return a function from the effect that removes the event listener
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  return width < breakpoint ? (
     <>
       {warehouse && (
         <section className="mainBlock">
-          <div className="warehoseDetail-header">
+          <div className="warehouseDetails-header">
             <img
-              className="warehouseDetail-header__back"
+              className="warehouseDetails-header__back"
               src={Back}
               alt="pencil"
             ></img>
             <h3 className="warehouseDetails__headerText">
               {warehouse.warehouse_name}
             </h3>
-            <img
-              className="warehouseDetail-header__edit"
-              src={Edit}
-              alt="pencil"
-            ></img>
+            <Icon
+              className="warehouseDetails-header__edit"
+              //   src={Edit}
+              //   alt="pencil"
+              fill="white"
+            ></Icon>
           </div>
-          <div className="warehoseDetail-contact">
-            <div>
-              <h5 className="warehoseDetail-contact__header">
+          <div className="warehouseDetails-contact">
+            <div className="warehouseDetails-contact__first">
+              <h5 className="warehouseDetails-contact__header">
                 WAREHOUSE ADRESS:
               </h5>
-              <p className="warehoseDetail-contact__content">
+              <p className="warehouseDetails-contact__content">
                 {warehouse.address}
               </p>
             </div>
-            <div>
-              <h5 className="warehoseDetail-contact__header">CONTACT NAME:</h5>
-              <p className="warehoseDetail-contact__content">
-                {warehouse.contact_name}
-              </p>
-              <p className="warehoseDetail-contact__content">
-                {warehouse.contact_position}
-              </p>
-            </div>
-            <div>
-              <h5 className="warehoseDetail-contact__header">
-                CONTACT INFORMATION:
-              </h5>
-              <p className="warehoseDetail-contact__content">
-                {warehouse.contact_phone}
-              </p>
-              <p className="warehoseDetail-contact__content">
-                {warehouse.contact_email}
-              </p>
+
+            <div className="warehouseDetails-contact__second">
+              <div className="warehouseDetails-contact__third">
+                <h5 className="warehouseDetails-contact__header">
+                  CONTACT NAME:
+                </h5>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_name}
+                </p>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_position}
+                </p>
+              </div>
+
+              <div className="warehouseDetails-contact__third">
+                <h5 className="warehouseDetails-contact__header">
+                  CONTACT INFORMATION:
+                </h5>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_phone}
+                </p>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_email}
+                </p>
+              </div>
             </div>
           </div>
 
@@ -137,11 +116,11 @@ export default function WarehouseDetails() {
                         <h5 className="warehouseDetails__sort-headerText ">
                           Inventory ITEM
                         </h5>
-                        {/* <img
+                        <img
                           className="warehouseDetails__sort-sortIcon"
                           src={SortIcon}
                           alt="sort icon"
-                        /> */}
+                        />
                       </div>
                       <div className="warehouseDetails__appliance-wrapper">
                         <a href="">
@@ -215,6 +194,165 @@ export default function WarehouseDetails() {
                         </h5>
                       </div>
 
+                      <div className="warehouseDetails__actions-btns">
+                        <button>
+                          <img src={DeleteIcon} alt="delete icon" />
+                        </button>
+                        <button>
+                          <img src={Edit} alt="edit icon" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </section>
+          ) : (
+            <section className="warehouseDettails__container">
+              <p className="warehouseDettails__no-data">No inventory</p>
+            </section>
+          )}
+        </section>
+      )}
+    </>
+  ) : (
+    <>
+      {warehouse && (
+        <section className="mainBlock">
+          <div className="warehouseDetails-header">
+            <img
+              className="warehouseDetails-header__back"
+              src={Back}
+              alt="pencil"
+            ></img>
+            <h3 className="warehouseDetails__headerText">
+              {warehouse.warehouse_name}
+            </h3>
+            <Icon
+              className="warehouseDetails-header__edit"
+              src={Edit}
+              alt="pencil"
+            ></Icon>
+          </div>
+          <div className="warehouseDetails-contact">
+            <div className="warehouseDetails-contact__first">
+              <h5 className="warehouseDetails-contact__header">
+                WAREHOUSE ADRESS:
+              </h5>
+              <p className="warehouseDetails-contact__content">
+                {warehouse.address}
+              </p>
+            </div>
+            <div className="warehouseDetails-contact__second">
+              <div className="warehouseDetails-contact__third">
+                <h5 className="warehouseDetails-contact__header">
+                  CONTACT NAME:
+                </h5>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_name}
+                </p>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_position}
+                </p>
+              </div>
+
+              <div className="warehouseDetails-contact__third">
+                <h5 className="warehouseDetails-contact__header">
+                  CONTACT INFORMATION:
+                </h5>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_phone}
+                </p>
+                <p className="warehouseDetails-contact__content">
+                  {warehouse.contact_email}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="warehouseDetails__headerLarge">
+            <div className="warehouseDetails__headerLarge-box">
+              <h5 className="warehouseDetails__headerLarge-text">
+                Inventory ITEM
+              </h5>
+              <img
+                className="warehouseDetails__headerLarge-icon"
+                src={SortIcon}
+                alt="sort icon"
+              />
+            </div>
+            <div className="warehouseDetails__headerLarge-box">
+              <h5 className="warehouseDetails__headerLarge-text">STATUS</h5>
+              <img
+                className="warehouseDetails__headerLarge-icon"
+                src={SortIcon}
+                alt="sort icon"
+              />
+            </div>
+            <div className="warehouseDetails__headerLarge-box">
+              <h5 className="warehouseDetails__headerLarge-text">CATEGORY</h5>
+              <img
+                className="warehouseDetails__headerLarge-icon"
+                src={SortIcon}
+                alt="sort icon"
+              />
+            </div>
+            <div className="warehouseDetails__headerLarge-box">
+              <h5 className="warehouseDetails__headerLarge-text">QTY</h5>
+              <img
+                className="warehouseDetails__headerLarge-icon"
+                src={SortIcon}
+                alt="sort icon"
+              />
+            </div>
+            <div className="warehouseDetails__actions-headerLarge">
+              <h5 className="warehouseDetails__headerLarge-text">ACTIONS</h5>
+            </div>
+          </div>
+
+          {inventories.length > 0 ? (
+            <section className="warehouseDetails">
+              {inventories.map((inventory) => (
+                <div className="warehouseDetails__list" key={inventory.id}>
+                  <div className="warehouseDetails__card-wrapper">
+                    {/* warehouseDetails ITEM */}
+                    <div className="warehouseDetails__item-wrapper">
+                      <div className="warehouseDetails__appliance-wrapper">
+                        <a href="">
+                          <p className="warehouseDetails__applianceText">
+                            {inventory.item_name}
+                          </p>
+                          <img src={Chevron} alt="right arrow" />
+                        </a>
+                      </div>
+                    </div>
+
+                    {/* warehouseDetails STATUS */}
+                    <div className="warehouseDetails__status-wrapper">
+                      <p className="warehouseDetails__statusText">
+                        {inventory.status}
+                      </p>
+                    </div>
+
+                    {/* warehouseDetails CATEGORY */}
+                    <div className="warehouseDetails__category-wrapper">
+                      <p className="warehouseDetails__categoryText">
+                        {inventory.category}
+                      </p>
+                    </div>
+
+                    {/* warehouseDetails QTY */}
+                    <div className="warehouseDetails__qty-wrapper">
+                      <p className="warehouseDetails__qtyText">
+                        {" "}
+                        {inventory.quantity}
+                      </p>
+                    </div>
+                    {/* warehouseDetails WAREHOUSE */}
+                    <div className="warehouseDetails__warehouse-wrapper"></div>
+
+                    {/* warehouseDetails ACTIONS BUTTONS */}
+                    <div className="warehouseDetails__actions">
                       <div className="warehouseDetails__actions-btns">
                         <button>
                           <img src={DeleteIcon} alt="delete icon" />
