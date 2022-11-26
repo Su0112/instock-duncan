fd7da4c97d958aea3ea6f8fa887fdde6fc32ba6b
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-11-25 20:32:18 -0800

    add API call

--- src/components/editWarehouse/EditWarehouse.js
+++ src/components/editWarehouse/EditWarehouse.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import React from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 // import { useForm } from "react-hook-form";
 
 const URL = "http://localhost:8080/warehouses/";
@@ -25,6 +26,44 @@ export default function EditWarehouse() {
       .catch((error) => console.log(error));
     // console.log(warehouse);
   }, [params]);
+
+  const [warehouseName, setWarehouseName] = useState("");
+  const [warehouseAddress, setWarehouseAddress] = useState("");
+  const [warehouseCity, setWarehouseCity] = useState("");
+  const [warehouseCountry, setWarehouseCountry] = useState("");
+  const [warehouseContactName, setWarehouseContactName] = useState("");
+  const [warehouseContactPosition, setWarehouseContactPosition] = useState("");
+  const [warehouseContactPhone, setWarehouseContactPhone] = useState("");
+  const [warehouseContactEmail, setWarehouseContactEmail] = useState("");
+
+  let navigate = useNavigate();
+
+  const updateWarehouse = (warehouseId) => {
+    navigate("/warehouses/" + warehouseId);
+  };
+
+  const handleSubmit = (event) => {
+    console.log("hello");
+    event.preventDefault();
+    axios
+      .put(URL + warehouseId, {
+        warehouse_name: warehouseName,
+        address: warehouseAddress,
+        city: warehouseCity,
+        country: warehouseCountry,
+        contact_name: warehouseContactName,
+        contact_position: warehouseContactPosition,
+        contact_phone: warehouseContactPhone,
+        contact_email: warehouseContactEmail,
+      })
+      .then((response) => {
+        console.log(response.data);
+        if (response.status === 200) {
+          updateWarehouse(warehouseId);
+        }
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <>
@@ -54,6 +93,11 @@ export default function EditWarehouse() {
                     id="name"
                     placeholder=""
                     defaultValue={warehouse.warehouse_name}
+                    // value={}
+                    onChange={(e) => setWarehouseName(e.target.value)}
+                    // onChange={(event) =>
+                    //   // setUpdatedWarehouse(event.target.value)
+                    // }
                   ></input>
                   <p className="form__input-title">Street Address</p>
                   <input
@@ -62,6 +106,7 @@ export default function EditWarehouse() {
                     id="adress"
                     placeholder=""
                     defaultValue={warehouse.address}
+                    onChange={(e) => setWarehouseAddress(e.target.value)}
                   ></input>
 
                   <p className="form__input-title">City</p>
@@ -70,6 +115,7 @@ export default function EditWarehouse() {
                     className="form__input"
                     id="adress"
                     defaultValue={warehouse.city}
+                    onChange={(e) => setWarehouseCity(e.target.value)}
                   ></input>
                   <p className="form__input-title">Country</p>
                   <input
@@ -77,6 +123,7 @@ export default function EditWarehouse() {
                     className="form__input"
                     id="adress"
                     defaultValue={warehouse.country}
+                    onChange={(e) => setWarehouseCountry(e.target.value)}
                   ></input>
                 </div>
               </section>
@@ -92,6 +139,7 @@ export default function EditWarehouse() {
                     id="contacName"
                     placeholder=""
                     defaultValue={warehouse.contact_name}
+                    onChange={(e) => setWarehouseContactName(e.target.value)}
                   ></input>
                   <p className="form__input-title">Position</p>
                   <input
@@ -100,6 +148,9 @@ export default function EditWarehouse() {
                     id="adress"
                     placeholder=""
                     defaultValue={warehouse.contact_position}
+                    onChange={(e) =>
+                      setWarehouseContactPosition(e.target.value)
+                    }
                   ></input>
                   <p className="form__input-title">Phone Number</p>
                   <input
@@ -107,6 +158,7 @@ export default function EditWarehouse() {
                     className="form__input"
                     id="phoneNumber"
                     defaultValue={warehouse.contact_phone}
+                    onChange={(e) => setWarehouseContactPhone(e.target.value)}
                   ></input>
                   <p className="form__input-title">Email</p>
                   <input
@@ -114,6 +166,7 @@ export default function EditWarehouse() {
                     className="form__input"
                     id="email"
                     defaultValue={warehouse.contact_email}
+                    onChange={(e) => setWarehouseContactEmail(e.target.value)}
                   ></input>
                 </div>
               </section>
@@ -123,7 +176,9 @@ export default function EditWarehouse() {
                 <button className="form__btn--cancel">Cancel</button>
               </div>
               <div className="form__btn-item">
-                <button className="form__btn--addItem">Save</button>
+                <button className="form__btn--addItem" onClick={handleSubmit}>
+                  Save
+                </button>
               </div>
             </div>
           </form>
