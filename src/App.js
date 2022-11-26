21eb7b5d8c345990b77d10a2595e51d1a891eef2
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-11-25 17:33:04 -0800

    added changes to App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import Header from "./components/header/Header";
 import InventoryList from "./components/inventory/InventoryList";
 import AddInventoryItem from "./components/addInventoryItem/AddInventoryItem";
 import InventoryItemDetails from "./components/inventoryItemDetails/InventoryItemDetails";
+<<<<<<< HEAD
 
 import WarehouseList from "./components/warehouseList/WarehouseList";
 import WarehouseDetails from "./components/warehouseDetails/WarehouseDetails";
+=======
+import WarehouseList from "./components/warehouseList/WarehouseList";
+>>>>>>> 240c35b (added changes to App.js)
 import "./App.scss";
 
 function App() {
@@ -19,7 +23,10 @@ function App() {
               <Route path="/" element={<WarehouseList />} />
               <Route path="/inventory" element={<InventoryList />} />
               <Route path="/addInventoryItem" element={<AddInventoryItem />} />
+<<<<<<< HEAD
               <Route path="/:warehouseId" element={<WarehouseDetails />} />
+=======
+>>>>>>> 240c35b (added changes to App.js)
               {/* add for editInventoryItem */}
               <Route
                 path="/inventoryDetails"
@@ -33,5 +40,4 @@ function App() {
     </>
   );
 }
-
 export default App;
