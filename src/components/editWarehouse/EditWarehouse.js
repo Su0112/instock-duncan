f150649f8ed5cca775809b14dedd6c6b3e775699
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-11-27 18:27:51 -0800

    change class name

--- src/components/editWarehouse/EditWarehouse.js
+++ src/components/editWarehouse/EditWarehouse.js
@@ -74,8 +74,6 @@ export default function EditWarehouse() {
   };
 
   const handleSubmit = (event) => {
-    console.log("hello");
-    console.log(warehouseContactPosition);
     event.preventDefault();
 
     console.log("Test " + warehouseAddress);
@@ -96,122 +94,126 @@ export default function EditWarehouse() {
       ? warehouseContactEmail
       : warehouse.contact_email;
 
-    console.log(
-      "Result " + address + city + name + country + contactN + position + email
-    );
+    // console.log(
+    //   "Result " + address + city + name + country + contactN + position + email
+    // );
 
-    // axios
-    //   .put(URL + warehouseId, {
-    //     warehouse_name: warehouseName,
-    //     address: warehouseAddress,
-    //     city: warehouseCity,
-    //     country: warehouseCountry,
-    //     contact_name: warehouseContactName,
-    //     contact_position: warehouseContactPosition,
-    //     contact_phone: warehouseContactPhone,
-    //     contact_email: warehouseContactEmail,
-    //   })
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     if (response.status === 200) {
-    //       updateWarehouse(warehouseId);
-    //     }
-    //   })
-    //   .catch((error) => console.log(error));
+    axios
+      .put(URL + warehouseId, {
+        warehouse_name: name,
+        address: address,
+        city: city,
+        country: country,
+        contact_name: contactN,
+        contact_position: position,
+        contact_phone: phone,
+        contact_email: email,
+      })
+      .then((response) => {
+        console.log(response.data);
+        if (response.status === 200) {
+          updateWarehouse(warehouseId);
+        }
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
     <>
-      <header className="form__header">
+      <header className="formWarehouse">
         <img
-          className="form__header-img"
+          className="formWarehouse-img"
           src={ArrowIcon}
           alt="Back arrow icon"
         ></img>
 
-        <h1 className="form__header-title">Edit Warehouse</h1>
+        <h1 className="formWarehouse-title">Edit Warehouse</h1>
       </header>
 
       {warehouse && (
         <main className="main">
           <form>
-            <section className="form__main">
-              <section className="form__warehouse-detailsFirst">
-                <div className="form__input-wrapper">
-                  <h2 className="form__warehouse-details-title">
+            <section className="formWarehouse__main">
+              <section className="formWarehouse__warehouse-detailsFirst">
+                <div className="formWarehouse__input-wrapper">
+                  <h2 className="formWarehouse_warehouse-details-title">
                     Warehouse Details
                   </h2>
-                  <label className="form__input-title">Warehouse Name</label>
+                  <label className="formWarehouse__input-title">
+                    Warehouse Name
+                  </label>
                   <input
                     name="warehouseName"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="warehouseName"
                     defaultValue={warehouse.warehouse_name}
                     onChange={handleChangeWarehouseName}
                   ></input>
-                  <p className="form__input-title">Street Address</p>
+                  <p className="formWarehouse__input-title">Street Address</p>
                   <input
                     name="warehouseAddress"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="warehouseAddress"
                     defaultValue={warehouse.address}
                     onChange={handleChangeWarehouseAddress}
                   ></input>
 
-                  <label className="form__input-title">City</label>
+                  <label className="formWarehouse__input-title">City</label>
                   <input
                     name="warehouseCity"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="warehouseCity"
                     defaultValue={warehouse.city}
                     onChange={handleChangeWarehouseCity}
                   ></input>
-                  <label className="form__input-title">Country</label>
+                  <label className="formWarehouse__input-title">Country</label>
                   <input
                     name="warehouseCountry"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="warehouseCountry"
                     defaultValue={warehouse.country}
                     onChange={handleChangeWarehouseCountry}
                   ></input>
                 </div>
               </section>
-              <section className="form__warehouse-detailsSecond">
-                <div className="form__input-wrapper">
-                  <h2 className="form__warehouse-details-title">
+              <section className="formWarehouse__warehouse-detailsSecond">
+                <div className="formWarehouse_input-wrapper">
+                  <h2 className="formWarehouse__warehouse-details-title">
                     Contact Details
                   </h2>
-                  <label className="form__input-title">Contact Name</label>
+                  <label className="formWarehouse__input-title">
+                    Contact Name
+                  </label>
                   <input
                     name="contactName"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="contacName"
                     placeholder="Contact Name"
                     value={warehouseContactName}
                     onChange={handleChangeContactName}
                   ></input>
-                  <label className="form__input-title">Position</label>
+                  <label className="formWarehouse__input-title">Position</label>
                   <input
                     name="ContactPosition"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="ContactPosition"
                     placeholder="Position"
                     value={warehouseContactPosition}
                     onChange={handleChangeContactPosition}
                   ></input>
-                  <p className="form__input-title">Phone Number</p>
+                  <p className="formWarehouse__input-title">Phone Number</p>
                   <input
                     name="warehouseContactPhone"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="warehouseContactPhone"
                     placeholder="Phone Number"
                     value={warehouseContactPhone}
                     onChange={handleChangeContactPhone}
                   ></input>
-                  <p className="form__input-title">Email</p>
+                  <p className="formWarehouse__input-title">Email</p>
                   <input
                     name="warehouseContactEmail"
-                    className="form__input"
+                    className="formWarehouse__input"
                     id="warehouseContactEmail"
                     placeholder="Email"
                     value={warehouseContactEmail}
@@ -220,12 +222,15 @@ export default function EditWarehouse() {
                 </div>
               </section>
             </section>
-            <div className="form__btn-wrapper">
-              <div className="form__btn-item">
-                <button className="form__btn--cancel">Cancel</button>
+            <div className="formWarehouse__btn-wrapper">
+              <div className="formWarehouse__btn-item">
+                <button className="formWarehouse__btn--cancel">Cancel</button>
               </div>
-              <div className="form__btn-item">
-                <button className="form__btn--addItem" onClick={handleSubmit}>
+              <div className="formWarehouse__btn-item">
+                <button
+                  className="formWarehouse__btn--addItem"
+                  onClick={handleSubmit}
+                >
                   Save
                 </button>
               </div>
